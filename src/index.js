6119b4893d4fc2960b425f24184f23935f4cfea0
akash yadav <48587080+akash0112@users.noreply.github.com>
2021-11-16 20:09:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import reducers from "./reducers/reducer";
+import reducers from "./reducers/combinereducer";
 const store = createStore(reducers);
 ReactDOM.render(
   <React.StrictMode>
